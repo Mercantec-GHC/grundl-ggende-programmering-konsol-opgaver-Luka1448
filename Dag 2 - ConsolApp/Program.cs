4a47701c929f8233fb7074266b4052a6defff4e8
Luka1448 <148041217+Luka1448@users.noreply.github.com>
2023-10-17 12:36:53 +0200

    Implement a method of the Math class that returns the larger of two numbers

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,4 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//int firstValue = 500;
+//int secondValue = 600;
+//int largerValue = Math.Max(firstValue, secondValue);
 
+//Console.WriteLine(largerValue);
 
+//-----------------------------------------------------------------------------------
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
