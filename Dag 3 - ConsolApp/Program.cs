5639eae143c8633f7eb428aab9c9cd7754ca2522
Luka1448 <148041217+Luka1448@users.noreply.github.com>
2023-10-18 21:18:39 +0200

    Code challenge: rewrite if-elseif-else using a switch statement

--- Dag 3 - ConsolApp/Program.cs
+++ Dag 3 - ConsolApp/Program.cs
@@ -1,100 +1,86 @@
-﻿//string value1 = " a";
-//string value2 = "A ";
-//Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+﻿//int employeeLevel = 100;
+//string employeeName = "John Smith";
 
-//Console.WriteLine("a" != "a");
-//Console.WriteLine("a" != "A");
-//Console.WriteLine(1 != 2);
+//string titel = "";
 
-//string myValue = "a";
-//Console.WriteLine(myValue != "a");
-
-//Console.WriteLine(1 > 2);
-//Console.WriteLine(1 < 2);
-//Console.WriteLine(1 >= 1);
-//Console.WriteLine(1 <= 1);
-
-//string pangram = "The quick brown fox jumps over the lazy dog.";
-//Console.WriteLine(pangram.Contains("fox"));
-//Console.WriteLine(pangram.Contains("cow"));
-
-//string pangram = "The quick brown fox jumps over the lazy dog.";
-//Console.WriteLine(!pangram.Contains("fox"));
-//Console.WriteLine(!pangram.Contains("cow"));
-
-//int saleAmount = 1001;
-//int discount = saleAmount > 1000 ? 100 : 50;
-//Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
-
-
-//Random number = new Random();
-//int flip1 = number.Next(1, 3);
-//string side = flip1 > 1 ? "heads" : "tails";
-//Console.WriteLine($"It's: {side}");
-
-
-//string permission = "Admin|Manager";
-//int level = 2;
-
-//if (permission.Contains("Admin"))
+//switch (employeeLevel)
 //{
-//    if (level > 55)
-//    {
-//        Console.WriteLine("Welcome, Super Admin user.");
-//    }
-//    else
-//    {
-//        Console.WriteLine("Welcome, Admin user.");
-//    }
-//}
-//else if (permission.Contains("Manager"))
-//{
-//    if (level >= 20)
-//    {
-//        Console.WriteLine("Contact an Admin for access.");
-//    }
-//    else
-//    {
-//        Console.WriteLine("You do not have sufficient privileges.");
-//    }
-//}
-//else
-//{
-//    Console.WriteLine("You do not have sufficient privileges.");
+//    case 100:
+//    case 200:
+//        titel = "Senior Associate";
+//        break;
+//    case 300:
+//        titel = "Manager";
+//        break;
+//    case 400:
+//        titel = "Senior Manager";
+//        break;
+//    default:
+//        titel = "Associate";
+//        break;
 //}
 
-
-//bool flag = true;
-//int value = 0;
-
-//if (flag)
-//{
-//    value = 10;
-//    Console.WriteLine("Inside of code block: " + value);
-//}
-//Console.WriteLine("Outside of code block: " + value);
+//Console.WriteLine($"{employeeName}, {titel}");
 
 
-//string name = "steve";
-//if (name == "bob") Console.WriteLine("Found Bob");
-//else if (name == "steve") Console.WriteLine("Found Steve");
-//else Console.WriteLine("Found Chuck");
-
-//int[] numbers = { 4, 8, 15, 16, 23, 42 };
-//int total = 0;
-//bool found = false;
-
-//foreach (int number in numbers)
-//{
-//    total += number;
-//    if (number == 42)
-//        found = true;
-//}
-
-//if (found)
-//    Console.WriteLine("Set contains 42");
-
-//Console.WriteLine($"Total: {total}");
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
 
 
 
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+
+switch (product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "Sweat shirt";
+        break;
+    case "03":
+        type = "Sweat shirt";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        type = "White";
+        break;
+}
+
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
