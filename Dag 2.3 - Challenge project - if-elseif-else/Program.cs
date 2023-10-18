1fbc2757a52fed60fdc02a7445f012c581b0f1d3
Luka1448 <148041217+Luka1448@users.noreply.github.com>
2023-10-18 08:40:14 +0200

    Challenge project - if-elseif-else - Done

--- Dag 2.3 - Challenge project - if-elseif-else/Program.cs
+++ Dag 2.3 - Challenge project - if-elseif-else/Program.cs
@@ -3,27 +3,33 @@
 int dayUntilExpiration = random.Next(12);
 int discountPercentage = 0;
 
-dayUntilExpiration = 11;
-
-Console.WriteLine(dayUntilExpiration);
+//dayUntilExpiration = 0;
+//Console.WriteLine(dayUntilExpiration);
 
 if (dayUntilExpiration > 10)
 {
-    Console.WriteLine("NEJ");
+
 }
-if (dayUntilExpiration >= 6)
+else if (dayUntilExpiration > 5)
 {
     Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
 else if (dayUntilExpiration > 1)
 {
-    Console.WriteLine("Your subscription expires in " + dayUntilExpiration + " days.\r\nRenew now and save 10%!");
+    Console.WriteLine("Your subscription expires in " + dayUntilExpiration + " days.");
+    discountPercentage = 10;
 }
 else if (dayUntilExpiration == 1)
 {
-    Console.WriteLine("Your subscription expieres within a day!\nRenew now and save 20%");
+    Console.WriteLine("Your subscription expieres within a day!");
+    discountPercentage = 20;
 }
 else if (dayUntilExpiration == 0)
 {
     Console.WriteLine("Your subscription has expired.");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine("Renew now and save " + discountPercentage + "%.");
 }
