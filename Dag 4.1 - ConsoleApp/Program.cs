858e2a9040b35b296aa04d2acb5f30d041cb266e
Luka1448 <148041217+Luka1448@users.noreply.github.com>
2023-10-21 15:14:25 +0200

    Exercise - Use the string's IndexOfAny() and LastIndexOf() helper methods

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -230,38 +230,124 @@
 
 //-------------------------------------------------------------------------------------
 
-string customerName = "Ms. Barros";
+//string customerName = "Ms. Barros";
 
-string currentProduct = "Magic Yield";
-int currentShares = 2975000;
-decimal currentReturn = 0.1275m;
-decimal currentProfit = 55000000.0m;
+//string currentProduct = "Magic Yield";
+//int currentShares = 2975000;
+//decimal currentReturn = 0.1275m;
+//decimal currentProfit = 55000000.0m;
 
-string newProduct = "Glorious Future";
-decimal newReturn = 0.13125m;
-decimal newProfit = 63000000.0m;
+//string newProduct = "Glorious Future";
+//decimal newReturn = 0.13125m;
+//decimal newProfit = 63000000.0m;
 
-// Your logic here
+//// Your logic here
 
-Console.WriteLine($"Dear {customerName}, \n\rAs a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
-Console.WriteLine($"Currently, you own {currentShares:C} shares at a return of {currentReturn:P2}.\n");
-Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+//Console.WriteLine($"Dear {customerName}, \n\rAs a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+//Console.WriteLine($"Currently, you own {currentShares:C} shares at a return of {currentReturn:P2}.\n");
+//Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
 
-Console.WriteLine("Here's a quick comparison:\n");
+//Console.WriteLine("Here's a quick comparison:\n");
 
-string comparisonMessage = "";
+//string comparisonMessage = "";
 
-comparisonMessage = currentProduct.PadRight(20);
-comparisonMessage += string.Format("{0:P}", currentReturn).PadRight(10);
-comparisonMessage += string.Format("{0:C}", currentProfit).PadRight(20);
+//comparisonMessage = currentProduct.PadRight(20);
+//comparisonMessage += string.Format("{0:P}", currentReturn).PadRight(10);
+//comparisonMessage += string.Format("{0:C}", currentProfit).PadRight(20);
 
-comparisonMessage += "\n";
-comparisonMessage += newProduct.PadRight(20);
-comparisonMessage += string.Format("{0:P}", newReturn).PadRight(10);
-comparisonMessage += string.Format("{0:C}", newProfit).PadRight(20);
+//comparisonMessage += "\n";
+//comparisonMessage += newProduct.PadRight(20);
+//comparisonMessage += string.Format("{0:P}", newReturn).PadRight(10);
+//comparisonMessage += string.Format("{0:C}", newProfit).PadRight(20);
 
-// Your logic here
+//// Your logic here
 
-Console.WriteLine(comparisonMessage);
+//Console.WriteLine(comparisonMessage);
 
 //-------------------------------------------------------------------------------------
+
+//string message = "What is the value <span>between the tags</span>?";
+
+//int openingPosition = message.IndexOf("<span>");
+//int closingPosition = message.IndexOf("</span>");
+
+//openingPosition += 6;
+//int length = closingPosition - openingPosition;
+//Console.WriteLine(message.Substring(openingPosition, length));
+
+//-------------------------------------------------------------------------------------
+
+//string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+//int openingPosition = message.LastIndexOf('(');
+
+//openingPosition += 1;
+//int closingPosition = message.LastIndexOf(')');
+//int length = closingPosition - openingPosition;
+//Console.WriteLine(message.Substring(openingPosition, length));
+
+//string message = "(What if) there are (more than) one (set of parentheses)?";
+//while (true)
+//{
+//    int openingPosition = message.IndexOf('(');
+//    if (openingPosition == -1) break;
+
+//    openingPosition += 1;
+//    int closingPosition = message.IndexOf(')');
+//    int length = closingPosition - openingPosition;
+//    Console.WriteLine(message.Substring(openingPosition, length));
+
+//    // Note the overload of the Substring to return only the remaining 
+//    // unprocessed message:
+//    message = message.Substring(closingPosition + 1);
+//}
+
+string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// The IndexOfAny() helper method requires a char array of characters. 
+// You want to look for:
+
+char[] openSymbols = { '[', '{', '(' };
+
+// You'll use a slightly different technique for iterating through 
+// the characters in the string. This time, use the closing 
+// position of the previous iteration as the starting index for the 
+//next open symbol. So, you need to initialize the closingPosition 
+// variable to zero:
+
+int closingPosition = 0;
+
+while (true)
+{
+    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+    if (openingPosition == -1) break;
+
+    string currentSymbol = message.Substring(openingPosition, 1);
+
+    // Now  find the matching closing symbol
+    char matchingSymbol = ' ';
+
+    switch (currentSymbol)
+    {
+        case "[":
+            matchingSymbol = ']';
+            break;
+        case "{":
+            matchingSymbol = '}';
+            break;
+        case "(":
+            matchingSymbol = ')';
+            break;
+    }
+
+    // To find the closingPosition, use an overload of the IndexOf method to specify 
+    // that the search for the matchingSymbol should start at the openingPosition in the string. 
+
+    openingPosition += 1;
+    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+    // Finally, use the techniques you've already learned to display the sub-string:
+
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+}
