371b92300b3dcedeb329e9cd367598cbcf9ab280
Luka1448 <148041217+Luka1448@users.noreply.github.com>
2023-10-21 14:53:49 +0200

    Exercise - Complete a challenge to apply string interpolation to a form letter

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -217,18 +217,51 @@
 //Console.WriteLine(input.PadLeft(12, '-'));
 //Console.WriteLine(input.PadRight(12, '-'));
 
-string paymentId = "769C";
-string payeeName = "Mr. Stephan ortega";
-string paymentAmount = "$5,000.00";
+//string paymentId = "769C";
+//string payeeName = "Mr. Stephan ortega";
+//string paymentAmount = "$5,000.00";
 
-var formettedLine = paymentId.PadRight(6);
-formettedLine += payeeName.PadRight(24);
-formettedLine += paymentAmount.PadLeft(10);
+//var formettedLine = paymentId.PadRight(6);
+//formettedLine += payeeName.PadRight(24);
+//formettedLine += paymentAmount.PadLeft(10);
 
-Console.WriteLine("1234567890123456789012345678901234567890");
-Console.WriteLine(formettedLine);
+//Console.WriteLine("1234567890123456789012345678901234567890");
+//Console.WriteLine(formettedLine);
 
 //-------------------------------------------------------------------------------------
 
+string customerName = "Ms. Barros";
 
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
 
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+
+Console.WriteLine($"Dear {customerName}, \n\rAs a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:C} shares at a return of {currentReturn:P2}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += string.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += string.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += string.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += string.Format("{0:C}", newProfit).PadRight(20);
+
+// Your logic here
+
+Console.WriteLine(comparisonMessage);
+
+//-------------------------------------------------------------------------------------
