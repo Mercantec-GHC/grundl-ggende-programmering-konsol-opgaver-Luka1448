dfcf54bb5bec2a1ea272ef44f32e15dd62365477
Luka1448 <148041217+Luka1448@users.noreply.github.com>
2023-10-23 14:54:11 +0200

    Dag 1

--- Dag 7 - BANKO/Program.cs
+++ Dag 7 - BANKO/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+public class MultiArr
+{
+    public static void Main(string[] args)
+    {
+        //BANKO info
+        string ID1 = "LukasB";
+        string ID2 = "Mikkel";
+        string ID3 = "";
+        string ID4 = "";
+        string ID5 = "";
+
+
+        int[,] LukasB = { { 40, 50, 62, 70, 80 }, { 7, 22, 36, 44, 52 }, { 8, 19, 38, 54, 66 } };
+        int[,] Mikkel = { { 12, 44, 60, 70, 81 }, { 4, 37, 45, 55, 72 }, { 6, 28, 48, 58, 65 } };
+
+//---------------------------------------------------------------------------------------------------------
+
+        //Display Info
+        Console.WriteLine($"ID1: {ID1}");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(LukasB[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+
+        Console.WriteLine($"ID2: {ID2}");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(Mikkel[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+
+//---------------------------------------------------------------------------------------------------------
+
+    }
+}
+
